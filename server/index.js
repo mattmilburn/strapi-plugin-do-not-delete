aca32e83f71698cb210b806a8409dc9fb515e206
Matt Milburn <me@mattmilburn.com>
2023-04-22 09:48:06 -0500

    Create initial server files

---
+++ server/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const bootstrap = require( './bootstrap' );
+const config = require( './config' );
+
+module.exports = {
+  bootstrap,
+  config,
+  // contentTypes,
+  // controllers,
+  // destroy() {},
+  // middlewares,
+  // policies,
+  // register() {},
+  // routes,
+  // services,
+};
