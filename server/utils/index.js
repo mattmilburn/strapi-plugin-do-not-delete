0e53ecfc396886d539f67b56ae3e794d811c38ad
Matt Milburn <me@mattmilburn.com>
2023-06-23 12:40:04 -0500

    Create isProtectedEntity util

--- server/utils/index.js
+++ server/utils/index.js
@@ -2,10 +2,12 @@
 
 const getService = require( './get-service' );
 const isApiRequest = require( './is-api-request' );
+const isProtectedEntity = require( './is-protected-entity' );
 const pluginId = require( './plugin-id' );
 
 module.exports = {
   getService,
   isApiRequest,
+  isProtectedEntity,
   pluginId,
 };
---
+++ server/utils/is-protected-entity.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const isProtectedEntity = ( entity, rules ) => rules.some( rule => {
+  const [ attr, comparator, value ] = rule;
+
+  switch ( comparator ) {
+    case 'is':
+      return entity[ attr ] === value;
+
+    case 'in':
+      return value.includes( entity[ attr ] );
+
+    case 'has':
+      return entity[ attr ].includes( value );
+
+    case 'matches':
+      return new RegExp( value ).test( entity[ attr ] );
+
+    default:
+      return false;
+  }
+} );
+
+module.exports = isProtectedEntity;
