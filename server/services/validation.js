045ff37c56b03597325d1b4d5d967437a3198aca
Matt Milburn <me@mattmilburn.com>
2023-06-23 12:40:22 -0500

    Refactor validation service to use isProtectedEntity

--- server/services/validation.js
+++ server/services/validation.js
@@ -2,36 +2,20 @@
 
 const { ValidationError } = require( '@strapi/utils' ).errors;
 
+const { isProtectedEntity } = require( '../utils' );
+
 module.exports = ( { strapi } ) => ( {
-  validate( entity, config ) {
-    if ( ! config.length ) {
+  validate( entity, rules ) {
+    if ( ! rules.length ) {
       return;
     }
 
-    // If any of the rules return true, this entry should NOT delete.
-    const isProtected = config.some( rule => {
-      const [ attr, comparator, value ] = rule;
-
-      switch ( comparator ) {
-        case 'is':
-          return entity[ attr ] === value;
-
-        case 'in':
-          return value.includes( entity[ attr ] );
-
-        case 'has':
-          return entity[ attr ].includes( value );
-
-        case 'matches':
-          return new RegExp( value ).test( entity[ attr ] );
-
-        default:
-          return false;
-      }
-    } );
-
-    if ( isProtected ) {
-      throw new ValidationError( 'This protected entry cannot be deleted.' );
+    // Do not delete if this is a protected entry.
+    if ( isProtectedEntity( entity, rules ) ) {
+      /**
+       * @TODO - i18n support for this error message.
+       */
+      throw new ValidationError( 'This entry is protected and cannot be deleted.' );
     }
   },
 } );
