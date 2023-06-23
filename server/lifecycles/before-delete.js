86196988dc319fa248d2518932ce75530369ba92
Matt Milburn <me@mattmilburn.com>
2023-06-23 12:41:19 -0500

    Extra comments

--- server/lifecycles/before-delete.js
+++ server/lifecycles/before-delete.js
@@ -14,6 +14,7 @@ module.exports = async ( { strapi } ) => {
     const { uid } = model;
     const { where } = params;
 
+    // Ensure the entity exists first.
     const entity = await strapi.db.query( uid ).findOne( { where } );
 
     if ( ! entity ) {
