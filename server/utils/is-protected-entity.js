b5569a43f2251c6c686eead528d07c065e3686d1
boris.shulyak <borysshulyak@gmail.com>
2023-07-12 12:38:17 +0300

    Run pretty.

--- server/utils/is-protected-entity.js
+++ server/utils/is-protected-entity.js
@@ -4,21 +4,21 @@ const COMPARATORS = {
   is: 'is',
   in: 'in',
   has: 'has',
-  matches: 'matches'
-}
+  matches: 'matches',
+};
 
 const COMPARATOR_ACTION_STRATEGY = {
   [COMPARATORS.is]: (value, entityAttr) => value === entityAttr,
   [COMPARATORS.in]: (value, entityAttr) => value.includes(entityAttr),
   [COMPARATORS.has]: (value, entityAttr) => entityAttr.includes(value),
-  [COMPARATORS.matches]: (value) => RegExp(value).test()
-}
+  [COMPARATORS.matches]: (value) => RegExp(value).test(),
+};
 
 const isProtectedEntity = (entity, rules) =>
   rules.some((rule) => {
     const [attr, comparator, value] = rule;
     const entityAttr = entity[attr];
-    return !!COMPARATOR_ACTION_STRATEGY[comparator](value, entityAttr)
+    return !!COMPARATOR_ACTION_STRATEGY[comparator](value, entityAttr);
   });
 
 module.exports = isProtectedEntity;
