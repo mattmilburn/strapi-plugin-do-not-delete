a519d2986637e8b3dcab6eccb00e1d3d10f4087e
boris.shulyak <borysshulyak@gmail.com>
2023-07-12 12:00:12 +0300

    Refactor `isProtectedEntity` method logic using Strategy pattern.

--- server/utils/is-protected-entity.js
+++ server/utils/is-protected-entity.js
@@ -1,26 +1,25 @@
 'use strict';
 
+const COMPARATORS = {
+  is: 'is',
+  in: 'in',
+  has: 'has',
+  matches: 'matches'
+}
+
+const COMPARATOR_ACTION_STRATEGY = {
+  [COMPARATORS.is]: (value, entityAttr) => value === entityAttr,
+  [COMPARATORS.in]: (value, entityAttr) => value.includes(entityAttr),
+  [COMPARATORS.has]: (value, entityAttr) => entityAttr.includes(value),
+  [COMPARATORS.has]: (value, entityAttr) => entityAttr.includes(value),
+  [COMPARATORS.matches]: (value) => RegExp(value).test()
+}
+
 const isProtectedEntity = (entity, rules) =>
   rules.some((rule) => {
     const [attr, comparator, value] = rule;
     const entityAttr = entity[attr];
-
-    switch (comparator) {
-      case 'is':
-        return entityAttr === value;
-
-      case 'in':
-        return value.includes(entityAttr);
-
-      case 'has':
-        return entityAttr.includes(value);
-
-      case 'matches':
-        return new RegExp(value).test();
-
-      default:
-        return false;
-    }
+    return COMPARATOR_ACTION_STRATEGY[comparator](value, entityAttr) || false;
   });
 
 module.exports = isProtectedEntity;
