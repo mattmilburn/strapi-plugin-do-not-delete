12626b34c6717937430fe1833747a47764920f0f
boris.shulyak <borysshulyak@gmail.com>
2023-07-12 12:52:28 +0300

    Implement `isNot`, `notIn`, `hasNot`  comparators.

--- server/utils/is-protected-entity.js
+++ server/utils/is-protected-entity.js
@@ -1,26 +1,31 @@
 'use strict';
 
+const COMPARATORS = {
+  is: 'is',
+  isNot: 'isNot',
+  in: 'in',
+  notIn: 'notIn',
+  has: 'has',
+  hasNot: 'hasNot',
+  matches: 'matches',
+};
+
+const COMPARATOR_ACTION_STRATEGY = {
+  [COMPARATORS.is]: (value, entityAttr) => value === entityAttr,
+  [COMPARATORS.isNot]: (value, entityAttr) => value !== entityAttr,
+  [COMPARATORS.in]: (value, entityAttr) => value.includes(entityAttr),
+  [COMPARATORS.notIn]: (value, entityAttr) => !value.includes(entityAttr),
+  [COMPARATORS.has]: (value, entityAttr) => entityAttr.includes(value),
+  [COMPARATORS.hasNot]: (value, entityAttr) => !entityAttr.includes(value),
+  [COMPARATORS.matches]: (value) => RegExp(value).test(),
+};
+
 const isProtectedEntity = (entity, rules) =>
   rules.some((rule) => {
     const [attr, comparator, value] = rule;
     const entityAttr = entity[attr];
 
-    switch (comparator) {
-      case 'is':
-        return entityAttr === value;
-
-      case 'in':
-        return value.includes(entityAttr);
-
-      case 'has':
-        return entityAttr.includes(value);
-
-      case 'matches':
-        return new RegExp(value).test(entityAttr);
-
-      default:
-        return false;
-    }
+    return !!COMPARATOR_ACTION_STRATEGY[comparator](value, entityAttr);
   });
 
 module.exports = isProtectedEntity;
