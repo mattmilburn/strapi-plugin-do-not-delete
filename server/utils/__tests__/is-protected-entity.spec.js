a444ecc5befaaba906851a376827fd086b93b331
Matt Milburn <me@mattmilburn.com>
2023-07-24 14:22:11 -0500

    Add tests for day, month, and year comparators

--- server/utils/__tests__/is-protected-entity.spec.js
+++ server/utils/__tests__/is-protected-entity.spec.js
@@ -117,7 +117,10 @@ describe('isProtectedEntity', () => {
 
   it('should return `true` if entity attribute matches the `lt` rule', () => {
     const entity = { attribute: 1 };
-    const rules = [['attribute', 'lt', 2]];
+    const rules = [
+      ['attribute', 'lt', 2],
+      ['attribute', 'lt', 10],
+    ];
     const result = isProtectedEntity(entity, rules);
 
     expect(result).toBe(true);
@@ -125,21 +128,32 @@ describe('isProtectedEntity', () => {
 
   it('should return `false` if entity attribute does not match the `lt` rule', () => {
     const entity = { attribute: 1 };
+    const rules = [
+      ['attribute', 'lt', 0],
+      ['attribute', 'lt', 1],
+    ];
+    const result = isProtectedEntity(entity, rules);
 
-    expect(isProtectedEntity(entity, [['attribute', 'lt', 1]])).toBe(false);
-    expect(isProtectedEntity(entity, [['attribute', 'lt', 0]])).toBe(false);
+    expect(result).toBe(false);
   });
 
   it('should return `true` if entity attribute matches the `lte` rule', () => {
     const entity = { attribute: 1 };
+    const rules = [
+      ['attribute', 'lte', 1],
+      ['attribute', 'lte', 2],
+    ];
+    const result = isProtectedEntity(entity, rules);
 
-    expect(isProtectedEntity(entity, [['attribute', 'lte', 1]])).toBe(true);
-    expect(isProtectedEntity(entity, [['attribute', 'lte', 2]])).toBe(true);
+    expect(result).toBe(true);
   });
 
   it('should return `false` if entity attribute does not match the `lte` rule', () => {
     const entity = { attribute: 1 };
-    const rules = [['attribute', 'lte', 0]];
+    const rules = [
+      ['attribute', 'lte', -1],
+      ['attribute', 'lte', 0],
+    ];
     const result = isProtectedEntity(entity, rules);
 
     expect(result).toBe(false);
@@ -147,7 +161,10 @@ describe('isProtectedEntity', () => {
 
   it('should return `true` if entity attribute matches the `gt` rule', () => {
     const entity = { attribute: 1 };
-    const rules = [['attribute', 'gt', 0]];
+    const rules = [
+      ['attribute', 'gt', -1],
+      ['attribute', 'gt', 0],
+    ];
     const result = isProtectedEntity(entity, rules);
 
     expect(result).toBe(true);
@@ -155,21 +172,32 @@ describe('isProtectedEntity', () => {
 
   it('should return `false` if entity attribute does not match the `gt` rule', () => {
     const entity = { attribute: 1 };
+    const rules = [
+      ['attribute', 'gt', 1],
+      ['attribute', 'gt', 2],
+    ];
+    const result = isProtectedEntity(entity, rules);
 
-    expect(isProtectedEntity(entity, [['attribute', 'gt', 1]])).toBe(false);
-    expect(isProtectedEntity(entity, [['attribute', 'gt', 2]])).toBe(false);
+    expect(result).toBe(false);
   });
 
   it('should return `true` if entity attribute matches the `gte` rule', () => {
     const entity = { attribute: 1 };
+    const rules = [
+      ['attribute', 'gte', 0],
+      ['attribute', 'gte', 1],
+    ];
+    const result = isProtectedEntity(entity, rules);
 
-    expect(isProtectedEntity(entity, [['attribute', 'gte', 1]])).toBe(true);
-    expect(isProtectedEntity(entity, [['attribute', 'gte', 0]])).toBe(true);
+    expect(result).toBe(true);
   });
 
   it('should return `false` if entity attribute does not match the `gte` rule', () => {
     const entity = { attribute: 1 };
-    const rules = [['attribute', 'gte', 2]];
+    const rules = [
+      ['attribute', 'gte', 2],
+      ['attribute', 'gte', 10],
+    ];
     const result = isProtectedEntity(entity, rules);
 
     expect(result).toBe(false);
@@ -177,22 +205,34 @@ describe('isProtectedEntity', () => {
 
   it('should return `true` if entity attribute matches the `between` rule', () => {
     const entity = { attribute: 1 };
+    const rules = [
+      ['attribute', 'between', [0, 2]],
+      ['attribute', 'between', [0, 1]],
+      ['attribute', 'between', [1, 2]],
+    ];
+    const result = isProtectedEntity(entity, rules);
 
-    expect(isProtectedEntity(entity, [['attribute', 'between', [0, 2]]])).toBe(true);
-    expect(isProtectedEntity(entity, [['attribute', 'between', [0, 1]]])).toBe(true);
-    expect(isProtectedEntity(entity, [['attribute', 'between', [1, 2]]])).toBe(true);
+    expect(result).toBe(true);
   });
 
   it('should return `false` if entity attribute does not match the `between` rule', () => {
     const entity = { attribute: 1 };
+    const rules = [
+      ['attribute', 'between', [-2, 0]],
+      ['attribute', 'between', [2, 4]],
+    ];
+    const result = isProtectedEntity(entity, rules);
 
-    expect(isProtectedEntity(entity, [['attribute', 'between', [-2, 0]]])).toBe(false);
-    expect(isProtectedEntity(entity, [['attribute', 'between', [2, 4]]])).toBe(false);
+    expect(result).toBe(false);
   });
 
   it('should return `true` if entity attribute matches the `after` rule', () => {
     const entity = { attribute: '2020-01-01' };
-    const rules = [['attribute', 'after', '2019-12-31']];
+    const rules = [
+      ['attribute', 'after', 'Tue, 31 Dec 2019 19:12:27 GMT'],
+      ['attribute', 'after', '2019-12-31T19:12:27.873Z'],
+      ['attribute', 'after', '2019-12-31'],
+    ];
     const result = isProtectedEntity(entity, rules);
 
     expect(result).toBe(true);
@@ -200,14 +240,24 @@ describe('isProtectedEntity', () => {
 
   it('should return `false` if entity attribute does not match the `after` rule', () => {
     const entity = { attribute: '2019-12-31' };
+    const rules = [
+      ['attribute', 'after', 'Tue, 31 Dec 2019 19:12:27 GMT'],
+      ['attribute', 'after', '2019-12-31T19:12:27.873Z'],
+      ['attribute', 'after', '2019-12-31'],
+      ['attribute', 'after', '2020-01-01'],
+    ];
+    const result = isProtectedEntity(entity, rules);
 
-    expect(isProtectedEntity(entity, [['attribute', 'after', '2019-12-31']])).toBe(false);
-    expect(isProtectedEntity(entity, [['attribute', 'after', '2020-01-01']])).toBe(false);
+    expect(result).toBe(false);
   });
 
   it('should return `true` if entity attribute matches the `before` rule', () => {
     const entity = { attribute: '2019-12-31' };
-    const rules = [['attribute', 'before', '2020-01-01']];
+    const rules = [
+      ['attribute', 'before', 'Tue, 31 Dec 2019 19:12:27 GMT'],
+      ['attribute', 'before', '2019-12-31T19:12:27.873Z'],
+      ['attribute', 'before', '2020-01-01'],
+    ];
     const result = isProtectedEntity(entity, rules);
 
     expect(result).toBe(true);
@@ -215,9 +265,93 @@ describe('isProtectedEntity', () => {
 
   it('should return `false` if entity attribute does not match the `before` rule', () => {
     const entity = { attribute: '2020-01-01' };
+    const rules = [
+      ['attribute', 'before', 'Tue, 31 Dec 2019 19:12:27 GMT'],
+      ['attribute', 'before', '2019-12-31T19:12:27.873Z'],
+      ['attribute', 'before', '2019-12-31'],
+      ['attribute', 'before', '2020-01-01'],
+    ];
+    const result = isProtectedEntity(entity, rules);
 
-    expect(isProtectedEntity(entity, [['attribute', 'before', '2019-12-31']])).toBe(false);
-    expect(isProtectedEntity(entity, [['attribute', 'before', '2020-01-01']])).toBe(false);
+    expect(result).toBe(false);
+  });
+
+  it('should return `true` if entity attribute matches the `day` rule', () => {
+    const entity = { attribute: '2020-01-01' };
+    const rules = [
+      ['attribute', 'day', 'Wed, 01 Jan 2020 19:12:27 GMT'],
+      ['attribute', 'day', '2020-01-01T19:12:27.873Z'],
+      ['attribute', 'day', '2020-01-01'],
+    ];
+    const result = isProtectedEntity(entity, rules);
+
+    expect(result).toBe(true);
+  });
+
+  it('should return `false` if entity attribute does not match the `day` rule', () => {
+    const entity = { attribute: '2019-01-01' };
+    const rules = [
+      ['attribute', 'day', 'Wed, 01 Jan 2020 19:12:27 GMT'],
+      ['attribute', 'day', '2020-01-01T19:12:27.873Z'],
+      ['attribute', 'day', '2020-01-01'],
+    ];
+    const result = isProtectedEntity(entity, rules);
+
+    expect(result).toBe(false);
+  });
+
+  it('should return `true` if entity attribute matches the `month` rule', () => {
+    const entity = { attribute: '2020-01-01' };
+    const rules = [
+      ['attribute', 'month', 'Fri, 31 Jan 2020 19:12:27 GMT'],
+      ['attribute', 'month', '2020-01-31T19:12:27.873Z'],
+      ['attribute', 'month', '2020-01-31'],
+      ['attribute', 'month', '2020-01'],
+    ];
+    const result = isProtectedEntity(entity, rules);
+
+    expect(result).toBe(true);
+  });
+
+  it('should return `false` if entity attribute does not match the `month` rule', () => {
+    const entity = { attribute: '2019-01-01' };
+    const rules = [
+      ['attribute', 'month', 'Wed, 01 Jan 2020 19:12:27 GMT'],
+      ['attribute', 'month', '2020-01-01T19:12:27.873Z'],
+      ['attribute', 'month', '2020-01-01'],
+      ['attribute', 'month', '2020-01'],
+    ];
+    const result = isProtectedEntity(entity, rules);
+
+    expect(result).toBe(false);
+  });
+
+  it('should return `true` if entity attribute matches the `year` rule', () => {
+    const entity = { attribute: '2020-01-01' };
+    const rules = [
+      ['attribute', 'year', 'Tue, 01 Dec 2020 19:12:27 GMT'],
+      ['attribute', 'year', '2020-12-01T19:12:27.873Z'],
+      ['attribute', 'year', '2020-12-01'],
+      ['attribute', 'year', '2020-12'],
+      ['attribute', 'year', '2020'],
+    ];
+    const result = isProtectedEntity(entity, rules);
+
+    expect(result).toBe(true);
+  });
+
+  it('should return `false` if entity attribute does not match the `year` rule', () => {
+    const entity = { attribute: '2019-01-01' };
+    const rules = [
+      ['attribute', 'year', 'Wed, 01 Jan 2020 19:12:27 GMT'],
+      ['attribute', 'year', '2020-01-01T19:12:27.873Z'],
+      ['attribute', 'year', '2020-01-01'],
+      ['attribute', 'year', '2020-01'],
+      ['attribute', 'year', '2020'],
+    ];
+    const result = isProtectedEntity(entity, rules);
+
+    expect(result).toBe(false);
   });
 
   it('should return `false` if the provided `comparator` is unknown', () => {
