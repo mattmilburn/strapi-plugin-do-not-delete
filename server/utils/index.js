9e6d618c060ea252b0baa5345176d02fe0241339
Matt Milburn <me@mattmilburn.com>
2024-04-07 09:54:07 -0500

    Remove unused isApiRequest util

--- server/utils/index.js
+++ server/utils/index.js
@@ -1,13 +1,11 @@
 'use strict';
 
 const getService = require('./get-service');
-const isApiRequest = require('./is-api-request');
 const isProtectedEntity = require('./is-protected-entity');
 const pluginId = require('./plugin-id');
 
 module.exports = {
   getService,
-  isApiRequest,
   isProtectedEntity,
   pluginId,
 };
